55cc5f7bcd3060da6f16fa4208d986fe071d2403
RandomJapaneseDude <83206060+inoue-773@users.noreply.github.com>
2024-03-18 04:27:10 +0900

    reimplement serverside log

--- bot.py
+++ bot.py
@@ -89,6 +89,8 @@ async def forcestop(ctx):
 async def givept(ctx, user: discord.User, amount: int):
     users_collection.update_one({"user_id": user.id}, {"$inc": {"points": amount}})
     await ctx.send(f"Given {amount} points to {user.name}.", ephemeral=True)
+    print(f"Error:{ctx.author.name} has given {amount} points to {user.name}")
+
 
 
 @bot.command()
@@ -102,6 +104,7 @@ async def takept(ctx, user: discord.User, amount: int):
 
     users_collection.update_one({"user_id": user.id}, {"$inc": {"points": -amount}})
     await ctx.send(f"Taken {amount} points from {user.name}.", ephemeral=True)
+    print(f"Error:{ctx.author.name} has taken {amount} points from {user.name}")
 
 
 @bot.command()
